6b412c7085a9a8645dda6121e91e190b7d3407b4
Darrik Crawford <darrikcrawford@yahoo.com>
2021-10-03 18:01:23 -0500

    created post data function

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   // Note that we save the animalsArray as filteredResults here:
@@ -63,6 +69,12 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
       res.sendStatus(404);
     }
+});
+
+app.post('/api/animals', (req, res) => {
+  // req.body is where our incoming content will be
+  console.log(req.body);
+  res.json(req.body);
 });
 
 app.listen(PORT, () => {
